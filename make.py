d57386caae688622ec7af9ece635b70a94840457
Gleb Polyakov <76563597+1GLEBOS1@users.noreply.github.com>
2025-06-01 17:57:11 +0300

    Updated structure of files

--- make.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def make():
-    os.system("cd files && unzip -o image.zip")
-    os.system("cd files && make")
--- script_1.js
+++
@@ -1,8 +0,0 @@
-form = new FormData();
-form.append("file", myFile);
-let response = await fetch('/uploadfile/', {
-    method: 'POST',
-    body: form
-    });
-
-    let result = await response.json();
